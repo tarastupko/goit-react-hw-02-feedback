94b4ef371783b53ae0191a48625be15ccb9017e5
tarastupko <109355475+tarastupko@users.noreply.github.com>
2023-07-28 14:46:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
 import { Notification } from "./Notification/Notification"
 
+
 export class App extends Component {
   state = {
     good: 0,
